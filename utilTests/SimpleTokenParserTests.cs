3d1229b992dcbce470b1fc1531653ecebd31d24b
katagata <haneimo@gmail.com>
2019-06-24 23:50:38 +0900

    各トークンパーサとテストのスケルトンを作成

---
+++ utilTests/SimpleTokenParserTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser.Tests
+{
+    [TestClass()]
+    public class SimpleTokenParserTests
+    {
+        [TestMethod()]
+        public void 出力文字列を結合した文字列と入力文字列を比較した際に内容一致すること()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ChunkWordの入力中にFixHalfWidthWordで指定した境界が到来した場合例外をスローすること()
+        {
+            Assert.Fail();
+        }
+
+    }
+}
